731fd55a6e292b4c75b5083c45a5721e3da2173e
error404124 <n.rud@g.nsu.ru>
2023-10-22 01:59:12 +0700

    Commit message "SecondTry"

--- src/test/java/edu/hw2/TestTask4.java
+++ src/test/java/edu/hw2/TestTask4.java
@@ -9,13 +9,13 @@ public class TestTask4 {
     @Test
     void testMethodName() {
         var ans = "main";
-        assertThat(ans).isEqualTo(Objects.requireNonNull(Task4.getMethodsAndClasses()).methodName());
+        assertThat(ans).isEqualTo(Objects.requireNonNull(Task4.getMethodsAndClasses().methodName()));
     }
 
     @Test
     void testClassName() {
         var ans = "com.intellij.rt.junit.JUnitStarter";
-        assertThat(ans).isEqualTo(Objects.requireNonNull(Task4.getMethodsAndClasses()).className());
+        assertThat(ans).isEqualTo(Task4.getMethodsAndClasses().className());
     }
 
     @Test
