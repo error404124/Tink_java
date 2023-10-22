64af08990fc5a26b85fee326719b3d2c6c4b89a2
error404124 <n.rud@g.nsu.ru>
2023-10-22 13:07:50 +0700

    Commit message "fix"

--- src/main/java/edu/project1/Main.java
+++ src/main/java/edu/project1/Main.java
@@ -1,14 +1,11 @@
 package edu.project1;
 
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-
 public final class Main {
 
     private Main() {
     }
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
     }
 }
