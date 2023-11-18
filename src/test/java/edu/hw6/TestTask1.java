e0f61e020945ed887f23b4d504a0a3c973d311ee
error404124 <n.rud@g.nsu.ru>
2023-11-19 03:39:33 +0700

    Commit message "tests"

--- src/test/java/edu/hw6/TestTask1.java
+++ src/test/java/edu/hw6/TestTask1.java
@@ -1,19 +1,108 @@
 package edu.hw6;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.HashMap;
+import java.util.Map;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TestTask1 {
-    @Test
-    void test1(){
-        Task1.DiskMap f = new Task1.DiskMap("Test");
-        String answer = "c";
-        assertThat(answer).isEqualTo(f.get("3"));
+    private static final String TEST_FILE_PATH = "testfile.txt";
+
+    @BeforeEach
+    void setUp() {
+        try {
+            Files.write(Path.of(TEST_FILE_PATH), "key1:value1\nkey2:value2\n".getBytes());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
+    @AfterEach
+    void tearDown() {
+        try {
+            Files.deleteIfExists(Path.of(TEST_FILE_PATH));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
-    void test2(){
-        Task1.DiskMap f = new Task1.DiskMap("Test");;
-        String answer = "{1=a, 2=b, 3=c, 4=d, 5=e, 6=f, 7=g}";
-        assertThat(answer).isEqualTo(f.loadMapFromFile().toString());
+    void testSize() {
+        Task1.DiskMap diskMap = new Task1.DiskMap(TEST_FILE_PATH);
+        assertEquals(2, diskMap.size());
+    }
+
+    @Test
+    void testIsEmpty() {
+        Task1.DiskMap diskMap = new Task1.DiskMap(TEST_FILE_PATH);
+        assertFalse(diskMap.isEmpty());
+    }
+
+    @Test
+    void testContainsKey() {
+        Task1.DiskMap diskMap = new Task1.DiskMap(TEST_FILE_PATH);
+        assertTrue(diskMap.containsKey("key1"));
+    }
+
+    @Test
+    void testContainsValue() {
+        Task1.DiskMap diskMap = new Task1.DiskMap(TEST_FILE_PATH);
+        assertTrue(diskMap.containsValue("value1"));
+    }
+
+    @Test
+    void testGet() {
+        Task1.DiskMap diskMap = new Task1.DiskMap(TEST_FILE_PATH);
+        assertEquals("value1", diskMap.get("key1"));
+    }
+
+    @Test
+    void testPut() {
+        Task1.DiskMap diskMap = new Task1.DiskMap(TEST_FILE_PATH);
+        diskMap.put("key3", "value3");
+        assertEquals(3, diskMap.size());
+    }
+
+    @Test
+    void testRemove() {
+        Task1.DiskMap diskMap = new Task1.DiskMap(TEST_FILE_PATH);
+        diskMap.remove("key1");
+        assertNull(diskMap.get("key1"));
+        assertEquals(1, diskMap.size());
+    }
+
+    @Test
+    void testPutAll() {
+        Task1.DiskMap diskMap = new Task1.DiskMap(TEST_FILE_PATH);
+        Map<String, String> newData = new HashMap<>();
+        newData.put("key3", "value3");
+        newData.put("key4", "value4");
+        diskMap.putAll(newData);
+        assertEquals(4, diskMap.size());
+    }
+
+
+    @Test
+    void testKeySet() {
+        Task1.DiskMap diskMap = new Task1.DiskMap(TEST_FILE_PATH);
+        assertEquals(2, diskMap.keySet().size());
+        assertTrue(diskMap.containsKey("key1"));
+        assertTrue(diskMap.containsKey("key2"));
+    }
+
+    @Test
+    void testValues() {
+        Task1.DiskMap diskMap = new Task1.DiskMap(TEST_FILE_PATH);
+        assertEquals(2, diskMap.values().size());
+        assertTrue(diskMap.containsValue("value1"));
+        assertTrue(diskMap.containsValue("value2"));
     }
 }
+
